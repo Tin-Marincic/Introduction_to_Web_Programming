26534d784e9b9e1767fa011291056a39d4a92674
Tin-Marincic <tin.marincic@stu.ibu.edu.ba>
2025-06-07 01:32:41 +0200

    edited constants so it supports both local and prod enviornment

--- frontend/utils/constants.js
+++ frontend/utils/constants.js
@@ -1,6 +1,9 @@
 let Constants = {
-   PROJECT_BASE_URL: "https://unisport-9kjwi.ondigitalocean.app/",
-   USER_ROLE: "user",
-   ADMIN_ROLE: "admin",
-   INSTRUCTOR_ROLE: "instructor"
-}
+  PROJECT_BASE_URL:
+    location.hostname === "localhost" || location.hostname === "127.0.0.1"
+      ? "http://localhost/TinMarincic/Introduction_to_Web_Programming/backend/"
+      : "https://unisport-9kjwi.ondigitalocean.app/",
+  USER_ROLE: "user",
+  ADMIN_ROLE: "admin",
+  INSTRUCTOR_ROLE: "instructor"
+};
