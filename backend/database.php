a2e1aa34e98f122ee0e72b0c062c6706f21cb223
Tin-Marincic <tin.marincic@stu.ibu.edu.ba>
2025-06-06 22:50:16 +0200

    edited database.php

--- backend/database.php
+++ backend/database.php
@@ -7,15 +7,18 @@ class Database {
     public static function connect() {
         if (self::$connection === null) {
             try {
+                $options = [
+                    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+                    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+                    PDO::MYSQL_ATTR_SSL_CA => '/etc/ssl/certs/ca-certificates.crt',  
+                    PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => false
+                ];
+
                 self::$connection = new PDO(
                     "mysql:host=" . Config::DB_HOST() . ";port=" . Config::DB_PORT() . ";dbname=" . Config::DB_NAME(),
                     Config::DB_USER(),
                     Config::DB_PASSWORD(),
-                    [
-                        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-                        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-                        PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => false
-                    ]
+                    $options
                 );
             } catch (PDOException $e) {
                 die("Connection failed: " . $e->getMessage());
@@ -24,6 +27,5 @@ class Database {
         return self::$connection;
     }
 }
-?> 
-
+?>
 
